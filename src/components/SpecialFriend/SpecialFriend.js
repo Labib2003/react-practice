90b22e1846577904cadbeaeea061b057fa577a9a
Labib2003 <labib.salimi30@gmail.com>
2022-03-24 15:03:26 +0600

    more styles added

--- src/components/SpecialFriend/SpecialFriend.js
+++ src/components/SpecialFriend/SpecialFriend.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const SpecialFriend = (props) => {
-    const {name, email, address, phone, website} = props.specialFriend;
+    const { name, email, address, phone, website } = props.specialFriend;
     return (
-        <div>
+        <div id='special-friend'>
             <h4>More details</h4>
             <p>Name: {name}</p>
             <p>Email: {email}</p>
-            <p>Address: {address?.suite}, {address?.street}, {address?.city}</p>
+            <p>Address: {address?.suite} {address?.street} {address?.city}</p>
             <p>Phone: {phone}</p>
             <p>Website: <a href={website}>{website}</a></p>
         </div>

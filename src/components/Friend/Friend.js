d86d71fef134ec70aad659141a34f1515d46e7e2
Labib2003 <labib.salimi30@gmail.com>
2022-03-25 12:20:17 +0600

    styles updated

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './Friend.css'
 
 const Friend = (props) => {
-    const {username, email, address, id} = props.friend;
+    const {username, email, id} = props.friend;
     const {showDetails} = props;
     return (
         <div className='friend'>
             <p>Username: {username}</p>
             <p>Email: {email}</p>
-            <p>Address: {address.suite}, {address.street}, {address.city}</p>
             <button className='more-details-btn' onClick={() => showDetails(id)}>More details</button>
         </div>
     );
--- src/components/SpecialFriend/SpecialFriend.js
+++ src/components/SpecialFriend/SpecialFriend.js
@@ -3,11 +3,11 @@ import React from 'react';
 const SpecialFriend = (props) => {
     const { name, email, address, phone, website } = props.specialFriend;
     return (
-        <div id='special-friend'>
+        <div className='special-friend'>
             <h4>More details</h4>
             <p>Name: {name}</p>
             <p>Email: {email}</p>
-            <p>Address: {address?.suite} {address?.street} {address?.city}</p>
+            <p>Address: {address?.suite}, {address?.street}, {address?.city}</p>
             <p>Phone: {phone}</p>
             <p>Website: <a href={website}>{website}</a></p>
         </div>
